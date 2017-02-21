05adff8b3e3206931b358ffd28a9da660f70c396
Tanzezhi <1633454306@qq.com>
2017-02-21 22:50:44 +0800

    仿qq界面布局

---
+++ app/src/main/java/com/example/lenovo/old_trading/Constant.java
@@ -0,0 +1,21 @@
+package com.example.lenovo.old_trading;
+
+/**
+ * Created by lenovo on 2017/2/20.
+ */
+public class Constant {
+    //Btn的标识
+    public static final int BTN_FLAG_MESSAGE = 0x01;
+    public static final int BTN_FLAG_CONTACTS = 0x01 << 1;
+    public static final int BTN_FLAG_NEWS = 0x01 << 2;
+    public static final int BTN_FLAG_SETTING = 0x01 << 3;
+
+    //Fragment的标识
+    public static final String FRAGMENT_FLAG_MESSAGE = "首页";
+    public static final String FRAGMENT_FLAG_CONTACTS = "我要买";
+    public static final String FRAGMENT_FLAG_NEWS = "我要卖";
+    public static final String FRAGMENT_FLAG_SETTING = "个人中心";
+    public static final String FRAGMENT_FLAG_SIMPLE = "simple";
+
+
+}
